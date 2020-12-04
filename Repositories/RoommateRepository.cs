36bf02f6d8adeab30de4672f34bc9e0f7991c844
Faith Magras <bdanielle092@gmail.com>
2020-12-04 13:55:28 -0600

    can can search for a roommate

--- Repositories/RoommateRepository.cs
+++ Repositories/RoommateRepository.cs
@@ -68,20 +68,44 @@ namespace Roommate43.Repositories
                 }
             }
         }
-       
-        //public Chore GetById(int id)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using(SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = "Select FirstName, LastName, RentPortion, MoveInDate, RoomId FROM Roommate 
-        //                LEFT JOIN Room on Roommate.RoodId = Room.id
-        //                WHERE Id = @Roommate.id";
-                        
-        //        }
-        //    }
-        //}
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"Select FirstName, LastName, RentPortion, MoveInDate, room.Id AS RoomId, room.Name AS RoomName, room.MaxOccupancy AS RoomMaxOccupancy FROM Roommate 
+                        JOIN Room on Roommate.RoomId = Room.id
+                        WHERE Roommate.Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Roommate roommate = null;
+
+                    if (reader.Read())
+                    {
+                        roommate = new Roommate
+                        {
+                            Id = id,
+                            Firstname = reader.GetString(reader.GetOrdinal("FirstName")),
+                            Lastname = reader.GetString(reader.GetOrdinal("LastName")),
+                            RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                            MovedInDate = reader.GetDateTime(reader.GetOrdinal("MoveInDate")),
+                            Room = new Room
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("RoomId")),
+                                Name = reader.GetString(reader.GetOrdinal("RoomName")),
+                                MaxOccupancy = reader.GetInt32(reader.GetOrdinal("RoomMaxOccupancy"))
+                            }
+                        };
+                    }
+                    reader.Close();
+                    return roommate;
+
+                }
+            }
+        }
     }
 }
